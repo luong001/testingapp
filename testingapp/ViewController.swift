2b8d0db64ca0553fe0c4b6077237650bcc33b1db
Ky Luong <kyluong09@ky-2.local>
2018-02-01 16:17:33 -0600

    Initial Commit

---
+++ testingapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testingapp
+//
+//  Created by Ky Luong on 2/1/18.
+//  Copyright © 2018 appfactory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
